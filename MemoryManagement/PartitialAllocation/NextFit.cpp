2b8132c050dbb40ec08cc6dfd5cc77e9237d4668
deveranthony <deveranthony5@gmail.com>
2025-09-15 04:04:45 -1100

    create NextFit

---
+++ MemoryManagement/PartitialAllocation/NextFit.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void NextFit(int blockSize[], int m, int processSize[], int n){
+    int* allocation = (int*)malloc(sizeof(int)*n);
+    int j = 0, t = m -1;
+    memset(allocation, -1, sizeof(allocation));
+
+    for(int i = 0; i < n ; i ++){
+        while (j < m)
+        {
+            /* code */
+            allocation[i] = j;
+            blockSize[j] -= processSize[i];
+            t= ( j -1 ) % m;
+            break;
+        }
+        if(t==j){
+            t = ( j -1 ) % m;
+            break;
+        }
+        j = ( j + 1) % m;
+    }
+
+    cout << "\nProcess No.\tProcess Size\tBlock no.\n";
+    for (int i = 0; i < n; i++) {
+        cout << " " << i + 1 << "\t\t\t\t" << processSize[i]
+            << "\t\t\t\t";
+        if (allocation[i] != -1)
+            cout << allocation[i] + 1;
+        else
+            cout << "Not Allocated";
+        cout << endl;
+    }
+}
+
+int main(){
+    int blockSize[] = { 5, 10, 20 };
+    int processSize[] = { 10, 20, 5 };
+    int m = sizeof(blockSize) / sizeof(blockSize[0]);
+    int n = sizeof(processSize) / sizeof(processSize[0]);
+
+    NextFit(blockSize, m, processSize, n);
+
+    return 0;
+}
